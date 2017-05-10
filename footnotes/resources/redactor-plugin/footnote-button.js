e9dd59ffac4cafe89d982cd80d9c6d66dbff96a4
arvid <as@vierbeuter.de>
2017-05-10 17:38:20 +0200

    add some inline-doc

--- footnotes/resources/redactor-plugin/footnote-button.js
+++ footnotes/resources/redactor-plugin/footnote-button.js
@@ -14,8 +14,10 @@ RedactorPlugins.footnotebutton = function () {
 		},
 
 		init: function () {
+			//	add button to toolbar
 			var sup = this.button.add('footnote', this.lang.get('footnote') + '<sup>123</sup>');
 
+			//	add callback function for the button to be invoked on each click
 			this.button.addCallback(sup, this.footnotebutton.formatSup);
 		},
 
