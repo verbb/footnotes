a4320878468e7b224e16031a71e9ba667e9b621b
arvid <as@vierbeuter.de>
2019-11-06 17:43:46 +0100

    change description text of anchor link setting; add German translations

--- src/translations/de/footnotes.php
+++ src/translations/de/footnotes.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    //  TODO: implement me!
+    'Enable anchor links' => 'Sprungmarken aktivieren',
+    'Settings' => 'Einstellungen',
+    'When disabled, the superscript numbers substituting footnote texts will be just plain text. When enabled, those superscript numbers will be anchors linking to the corresponding footnotes (assumed the footnotes in the template are marked up as targets accordingly).' => 'Wenn deaktivert, dann werden die hochgestellten Zahlen, welche die Fußnotentexte ersetzen, einfache Texte sein. Wenn aktiviert, dann werden eben jene hochgestellten Zahlen Ankerlinks sein und zu den entsprechenden Fußnoten verlinken (vorausgesetzt die Fußnoten sind im Template entsprechend als Sprungmarken ausgewiesen).',
 ];
