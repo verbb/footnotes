19ffc562068c341c7e7ef12985af77e58729b177
Josh Crawford <joshua@sgroup.com.au>
2024-05-27 14:52:16 +1000

    Update English translations.

--- src/translations/en/footnotes.php
+++ src/translations/en/footnotes.php
@@ -1,4 +1,9 @@
 <?php
 
 return [
+  'Enable anchor links' => 'Enable anchor links',
+  'Enable duplicate footnotes' => 'Enable duplicate footnotes',
+  'Footnotes' => 'Footnotes',
+  'When disabled, identical footnote texts will share the same footnote number and the texts will be combined into a single footnote. When enabled, each footnote text will be listed seperately (with its own footnote number) regardless of any equality to other footnotes.' => 'When disabled, identical footnote texts will share the same footnote number and the texts will be combined into a single footnote. When enabled, each footnote text will be listed seperately (with its own footnote number) regardless of any equality to other footnotes.',
+  'When disabled, the superscript numbers substituting footnote texts will be just plain text. When enabled, those superscript numbers will be anchors linking to the corresponding footnotes (assumed the footnotes in the template are marked up as targets accordingly).' => 'When disabled, the superscript numbers substituting footnote texts will be just plain text. When enabled, those superscript numbers will be anchors linking to the corresponding footnotes (assumed the footnotes in the template are marked up as targets accordingly).',
 ];
